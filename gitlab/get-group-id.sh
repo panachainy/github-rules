de3a531a058169331953436fad9883e0bc96681f
Panachai <panachai.ny@gmail.com>
2025-12-01 12:05:57 +0700

    style(gitlab): format .env.example and update variable references in get-group-id script

--- gitlab/get-group-id.sh
+++ gitlab/get-group-id.sh
@@ -6,5 +6,5 @@ echo "TOKEN: $TOKEN"
 # URL encode the group name (replace / with %2F)
 ENCODED_GROUP_NAME=$(echo "$GROUP_NAME" | sed 's/\//%2F/g')
 
-curl --header "PRIVATE-TOKEN: $TOKEN" \
-  "$GITLAB_URL/api/v4/groups?search=$ENCODED_GROUP_NAME"
+curl --header "PRIVATE-TOKEN: ${TOKEN}" \
+  "${GITLAB_URL}/api/v4/groups?search=${ENCODED_GROUP_NAME}"
